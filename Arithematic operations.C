99083ac812c3033297125aa681449c462410640a
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-10-27 17:47:27 +0530

    Add files via upload

---
+++ Arithematic operations.C
@@ -0,0 +1,14 @@
+// arithematic operators
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a two numbers:");
+	scanf("%d%d",&a,&b);
+	printf("%d+%d=%d\n",a,b,a+b);
+	printf("%d-%d=%d\n",a,b,a-b);
+	printf("%d*%d=%d\n",a,b,a*b);
+	printf("%d/%d=%d\n",a,b,a/b);
+	printf("%d%%%d=%d\n",a,b,a%b);
+	return 0;
+}
