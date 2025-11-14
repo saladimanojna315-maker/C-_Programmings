18ba843830652c1d985ed76165d726c377dce50f
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-14 18:46:48 +0530

    Add files via upload

---
+++ Area and perimeter of a square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    int area,perimeter;
+    scanf("%d",&a);
+    area=a*a;
+    perimeter=4*a;
+    printf("%d ",area);
+    printf("%d ",perimeter);
+    return 0;
+} 
