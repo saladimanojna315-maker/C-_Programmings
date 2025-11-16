7e2279e3f9e829cf0f20cf40fb1d4bf69ae41733
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-16 08:15:42 +0530

    Add files via upload

---
+++ a is greater than 100 or not.c
@@ -0,0 +1,13 @@
+//a is greater than 100 or not
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter a number:");
+	scanf("%d",&a);
+	if(a>100)
+	printf("a is greater than 100\n");
+	else
+	printf("END");
+	return 0;
+}
