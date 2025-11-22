b80daa460f69e590ac12efe67b68f5c2c127abb1
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-22 16:15:46 +0530

    Add files via upload

---
+++ Function Scope.c
@@ -0,0 +1,18 @@
+//Function Scope
+#include<stdio.h>
+void f1();
+int g=100;
+int main()
+{
+	int a=40;
+	printf("%d\n",a);
+	printf("%d\n",g);
+	void f1()
+	{
+		int b=30;
+		printf("%d\n",a);
+		printf("%d\n",b);
+		printf("%d\n",g);
+	}
+	
+}
