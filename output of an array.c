c87f790d8f00507c5d546ba26cb77775c018c98a
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-23 16:26:58 +0530

    Add files via upload

---
+++ output of an array.c
@@ -0,0 +1,12 @@
+//output of an array
+#include<stdio.h>
+int main()
+{
+	int a[5]={1,2,3,4,5},i;
+	printf("enter array elements are:\n");
+	for(i=0;i<5;i++)
+	{
+		printf("a[%d]=%d\n",i,a[i]);
+	}
+	return 0;
+}
