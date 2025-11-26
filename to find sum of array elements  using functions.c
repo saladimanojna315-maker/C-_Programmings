a58082d858db57f31c84fdcab2088e14ea8aef23
saladimanojna315-maker <saladimanojna315@gmail.com>
2025-11-26 15:41:43 +0530

    Add files via upload

---
+++ to find sum of array elements  using functions.c
@@ -0,0 +1,28 @@
+//write a c program to find sum of array elements using functions
+#include<stdio.h>
+void read (int[],int);
+int add(int[],int);
+int main()
+{
+	int a[10],n,sum;
+	printf("enter number of elements:");
+	scanf("%d",&n);
+	read(a,n);
+	sum=add(a,n);
+	printf("sum=%d\n",sum);
+	return 0;
+}
+void read(int a[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+}
+int add(int a[],int n)
+{
+	int i,sum=0;
+	for(i=0;i<n;i++)
+	sum=sum+a[i];
+	return sum;
+}
+
